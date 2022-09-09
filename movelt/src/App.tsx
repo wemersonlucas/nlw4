2c0b9a8b9c6e61b1b19b496b6884593187d12222
wemersonlucas <w.lucas_sp_19@hotmail.com>
2022-09-09 12:25:38 -0300

    Iniciando projeto

---
+++ movelt/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
+import './Styles/styles.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
